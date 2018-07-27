5a11abab970875b87c315ea1c70ee6465f0810d9
Bjørn-Petter Johannessen <bpjohannessen@users.noreply.github.com>
2018-07-27 22:58:49 +0200

    Update trondsenformula.php

--- trondsenformula.php
+++ trondsenformula.php
@@ -1,41 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>Trondsen Formula | Fettenajs.com</title>
 </head>
 <body>
   
-<?php
-
-$a = -7.58525;
-$b = 0.06617;
-$c = 0.0260967;
-$d = 0.00832;
-$e = 0.0088144;
-
-$age = @$_POST["age"];
-$bilirubin = @$_POST["bilirubin"];
-$alat = @$_POST["alat"];
-$gtt = @$_POST["gtt"];
-
-$calc = $a + $b*$age + $c*$bilirubin + $d*$alat + $e*$gtt;
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  echo $calc;
-} else {
-  echo "not submitted";
-}
-
-?>
-
-<form action="trondsenformula.php" method="POST">
+<form autocomplete="off">
 
 <table>
-<tr><td>Age</td><td><input method="text" name="age"></td></tr>
-<tr><td>Bilirubin</td><td><input method="text" name="bilirubin"></td></tr>
-<tr><td>ALAT</td><td><input method="text" name="alat"></td></tr>
-<tr><td>GTT</td><td><input method="text" name="gtt"></td></tr>
-<tr><td rowspan="2"><input type="submit"></td></tr>
+<tr><td>Age</td><td><input method="text" name="age" id="alder" onblur="calculate()"></td></tr>
+<tr><td>Bilirubin</td><td><input method="text" name="bilirubin" id="bilirubin" onblur="calculate()"></td></tr>
+<tr><td>ALAT</td><td><input method="text" name="alat" id="alat" onblur="calculate()"></td></tr>
+<tr><td>GTT</td><td><input method="text" name="gtt" id="gtt" onblur="calculate()"></td></tr>
+<tr><td><p id="result_text"></p></td><td><p id="result"></p></td></tr>
 </table>
 
 </form>
+
+<script>
+
+calculate = function()
+{	
+    var a = -7.58525;
+    var b = 0.06617;
+    var c = 0.0260967;
+    var d = 0.00832;
+    var e = 0.0088144;
+
+    var age = document.getElementById('alder').value;
+    var bilirubin = document.getElementById('bilirubin').value;
+    var alat = document.getElementById('alat').value;
+    var gtt = document.getElementById('gtt').value;
+    calc = a + b * parseInt(age) + c * parseInt(bilirubin) + d * parseInt(alat) + e * parseInt(gtt);
+
+
+	document.getElementById('result').value = a + b * parseInt(age) + c * parseInt(bilirubin) + d * parseInt(alat) + e * parseInt(gtt);
+    
+    if(isNaN(calc)) {
+        document.getElementById('result_text').innerHTML = "";
+        document.getElementById("result").innerHTML = "";
+    } else {
+        document.getElementById('result_text').innerHTML = "Result:";
+        document.getElementById("result").innerHTML = calc;
+    }
+
+}
+
+</script>
+</body>
+</html>
